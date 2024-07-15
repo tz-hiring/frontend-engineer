22ee575215bf9bf8e13345dd65152e4fa57a4724
Angus Clark <angus.c@transitionzero.org>
2024-07-15 15:41:41 +0100

    chore: remove files

--- src/Card.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Card.module.css';
-
-export default function Card() {
-  return <div>
-    <h2>Card</h2>
-    <p>Card content</p>
-    <button className={styles['button-text']}>Click me</button>
-  </div>
-}
